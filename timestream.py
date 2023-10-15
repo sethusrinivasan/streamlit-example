c19ab201da96d3d801255b9ddb75f5673156f72e
Sethu Srinivasan <sethusrinivasan@gmail.com>
2023-10-15 20:12:26 +0000

    regions

--- timestream.py
+++ timestream.py
@@ -5,26 +5,29 @@ from datetime import datetime
 
 st.title('Timestream Demo - Work in progress')
 
-df = pd.read_csv(
-    "https://raw.githubusercontent.com/awslabs/amazon-timestream-tools/mainline/sample_apps/data/sample.csv",
-    names=[
-        "ignore0",
-        "region",
-        "ignore1",
-        "az",
-        "ignore2",
-        "hostname",
-        "measure_kind",
-        "measure",
-        "ignore3",
-        "ignore4",
-        "ignore5",
-    ],
-    usecols=["region", "az", "hostname", "measure_kind", "measure"],
-)
-df["time"] = datetime.now()
-df.reset_index(inplace=True, drop=False)
+#df = pd.read_csv(
+#    "https://raw.githubusercontent.com/awslabs/amazon-timestream-tools/mainline/sample_apps/data/sample.csv",
+#    names=[
+#        "ignore0",
+#        "region",
+#        "ignore1",
+#        "az",
+#        "ignore2",
+#        "hostname",
+#        "measure_kind",
+#        "measure",
+#        "ignore3",
+#        "ignore4",
+#        "ignore5",
+#    ],
+#    usecols=["region", "az", "hostname", "measure_kind", "measure"],
+#)
+#df["time"] = datetime.now()
+#df.reset_index(inplace=True, drop=False)
 
+df = {
+  "regions": ['us-gov-west-1', 'ap-northeast-1', 'ap-southeast-2','eu-central-1', 'eu-west-1', 'us-east-1','us-east-2','us-west-2']
+}
 
 st.dataframe(df)  
 
